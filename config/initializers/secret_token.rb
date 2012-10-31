2c65ad9121377747fee05261cf6f1b3a3b71df7b
Qingfeng Cai <francois_cai@hotmail.com>
2012-10-31 13:28:14 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octopus::Application.config.secret_token = '8afbc7a18fcf7ed7a3053e8e69dbf604fb094fd09177012cd6245158b5ec34a97c2b8fcf416488c1d8314897d063848e7ab96d1d45ba537671c83198a7fea830'
